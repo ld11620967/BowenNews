313d97234b1ec6abe080d92d6e46994f3340bada
nilin <11620967@qq.com>
2017-11-16 12:03:58 +0800

    Change title name.

--- app/src/main/java/com/nilin/bowennews/MainActivity.kt
+++ app/src/main/java/com/nilin/bowennews/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
         // 先设置界面主题，不同的主题对应不同的界面风格
         CMSGUI.setTheme(DefaultTheme::class.java)
